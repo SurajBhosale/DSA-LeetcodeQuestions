fdde41e3dccba56e4834c8c01b561d702bd303b3
Suraj Bhosale <bhosalesuraj2000@gmail.com>
2024-02-05 16:00:06 +0530

    Optimal Solution updated

--- Arrays/LongestConsecutiveSequence.java
+++ Arrays/LongestConsecutiveSequence.java
@@ -32,56 +32,56 @@ public class LongestConsecutiveSequence {
     // return longest;
     // }
 
-    ----------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------
 
     // Better Approach - O(nlongn)
-    public static int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        int lastSmaller = Integer.MIN_VALUE;
-        int longest = 1;
-        int cnt = 0;
-        if (n < 1) {
-            return 0;
-        }
-        Arrays.sort(nums);
-        for (int i = 0; i < n; i++) {
-            if (nums[i] - 1 == lastSmaller) {
-                cnt += 1;
-                lastSmaller = nums[i];
-            } else if (lastSmaller != nums[i]) {
-                cnt = 1;
-                lastSmaller = nums[i];
-            }
+    // public static int longestConsecutive(int[] nums) {
+    // int n = nums.length;
+    // int lastSmaller = Integer.MIN_VALUE;
+    // int longest = 1;
+    // int cnt = 0;
+    // if (n < 1) {
+    // return 0;
+    // }
+    // Arrays.sort(nums);
+    // for (int i = 0; i < n; i++) {
+    // if (nums[i] - 1 == lastSmaller) {
+    // cnt += 1;
+    // lastSmaller = nums[i];
+    // } else if (lastSmaller != nums[i]) {
+    // cnt = 1;
+    // lastSmaller = nums[i];
+    // }
 
-            longest = Math.max(longest, cnt);
-        }
+    // longest = Math.max(longest, cnt);
+    // }
 
-        return longest;
-    }
+    // return longest;
+    // }
 
-    ----------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------
 
     // Optimal Solution - O(2n)
     public static int longestConsecutive(int[] nums) {
         int n = nums.length;
-        if(n==0){
+        if (n == 0) {
             return 0;
         }
         Set<Integer> set = new HashSet<>();
         int longest = 1;
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             set.add(nums[i]);
         }
 
-        for(int it : set){
-            if(!set.contains(it-1)){
+        for (int it : set) {
+            if (!set.contains(it - 1)) {
                 int x = it;
                 int cnt = 1;
 
-                while(set.contains(x+1)){
-                    x = x+1;
-                    cnt+=1;
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    cnt += 1;
                 }
 
                 longest = Math.max(cnt, longest);
